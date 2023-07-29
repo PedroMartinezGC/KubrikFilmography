e81e5a8f5fe0ac938e8d60f1bcb923673c4a317d
PedroMartinezGC <pedromartinezgcicuendez@gmail.com>
2023-07-29 11:16:02 +0200

    feat(page marker): Created page dynamic page marker.

--- filmographyPage.js
+++ filmographyPage.js
@@ -10,12 +10,9 @@ let current = 0;
 let target = 0;
 let ease = .05;
 
-//window.addEventListener('resize', init);
-
 images.forEach((img, index) => {
     img.style.backgroundImage = `url(./images/${index + 1}.jpg)`;
 });
-console.log(images)
 
 function lerp(start, end, t){
     return start * (1 - t) + end * t;
@@ -53,16 +50,28 @@ function animateImages(){
 
 init();
 animate();
+animateStageLineMarker();
 
 // Main page lines
 
-let mainLine1  = document.getElementById('main-line1');
-let mainLine2  = document.getElementById('main-line2');
-let mainLine3  = document.getElementById('main-line3');
-let mainLine4  = document.getElementById('main-line4');
-let mainTitle1 = document.getElementById('main-title1');
-let mainTitle2 = document.getElementById('main-title2');
-let mainTitle3 = document.getElementById('main-title3');
+let mainLine1               = document.getElementById('main-line1');
+let mainLine2               = document.getElementById('main-line2');
+let mainLine3               = document.getElementById('main-line3');
+let mainLine4               = document.getElementById('main-line4');
+let stageLineMarker         = document.getElementById('stage-line-marker');
+let stageLineMarkerLine     = document.getElementById('stage-line-marker-line');
+let stageLineMarkerTitle    = document.querySelector('#stage-line-marker-line .title-font');
+let stageLineMarkerShape    = document.getElementById('stage-line-marker-shape');
+let mainTitle1              = document.getElementById('main-title1');
+let mainTitle2              = document.getElementById('main-title2');
+let mainTitle3              = document.getElementById('main-title3');
+let stage0                  =document.getElementById('stage0');
+let stage1                  =document.getElementById('stage1');
+let stage2                  =document.getElementById('stage2');
+
+// Scroll listeners
+let currentScroll = 0;
+let authorBoxPosition = 0;
 
 showTitleAnimation();
 
@@ -101,7 +110,90 @@ function openFilmDescription(filmIndex){
     console.log((mainSliderWidth + 500) + 'px')
     let newWidth = ( mainSliderWidth + 500 ) + 'px';
     slider.style.width = newWidth;
-    slider.style.paddingRight = '500px';
-    
+    slider.style.paddingRight = '500px';   
+}
+
+function animateStageLineMarker(){
+    let width = 10;
+    let isAuthorBoxIntersecting = false;
+
+    window.addEventListener("scroll", async ()=> {
+        // Adapt stage-line-marker witdh
+        currentScroll = window.scrollY;
+        width = currentScroll + window.innerWidth/4;
+        stageLineMarker.style.width = width + 'px';
+
+        // Check if author-box is intersecting
+        isAuthorBoxIntersecting = await isElementIntersecting('author-box');
+
+        // Check author box position
+        const targetElement = document.getElementById('author-box');
+        authorBoxPosition = targetElement.getBoundingClientRect();
+
+        //Compare currentScroll with authorBox position, for check if it already passed away
+        stageLineMarker.style.width = (authorBoxPosition.x < currentScroll && !isAuthorBoxIntersecting) ? (currentScroll + window.innerWidth/4)+'px' : '0px';
+
+        //Colorize
+        if(checkIntersectionBetweenElements(stageLineMarker, stage0)){
+            console.log('stage0')
+            colorizeStageLineMarker('white', 'black', 'Stage 0');
+        } 
+        if(checkIntersectionBetweenElements(stageLineMarker, stage1)) {
+            console.log('stage1')
+            colorizeStageLineMarker('black', 'white', 'Stage 1');
+        } 
+        if(checkIntersectionBetweenElements(stageLineMarker, stage2)) {
+            console.log('stage2')
+            colorizeStageLineMarker('white', 'rgb(34, 85, 179)', 'Stage 2');
+        }
+
+        /* let stageLineMarkerPosition = stageLineMarker.getBoundingClientRect();
+        let stage0Position = stage0.getBoundingClientRect();
+        let stage1Position = stage1.getBoundingClientRect();
+        let stage2Position = stage2.getBoundingClientRect();
+        
+        console.log(stage1Position.right, 'stage1Right')
+        console.log(stageLineMarkerPosition.right, 'lineRight')
+
+        //let intersects = !(stageLineMarkerPosition.right < stage0Position.right || div1Rect.top > div2Rect.bottom);
+        let intersects = (stageLineMarkerPosition.right > stage1Position.left && stage1Position.right > stageLineMarkerPosition.right)
+        if(intersects) console.log('INTERSECTION') */
+    });
+}
+
+//Element 1 is crossing the element's 2 width horizontally
+function checkIntersectionBetweenElements(element1, element2){
+    element1Position = element1.getBoundingClientRect();
+    element2Position = element2.getBoundingClientRect();
+    let intersects = (element1Position.right > element2Position.left && element2Position.right > element1Position.right)
+    return intersects;
+}
+
+function colorizeStageLineMarker(backgroundColor, color, text){
+    stageLineMarkerLine.style.backgroundColor   = backgroundColor;
+    stageLineMarkerShape.style.borderBottomColor  = backgroundColor;
+    stageLineMarkerTitle.style.color            = color;
+    stageLineMarkerTitle.textContent            = text;
+}
+
+function isElementIntersecting(id){
+    return new Promise((resolve) => {
+    // Get the target element you want to observe
+    const targetElement = document.getElementById(id);
+
+    // Create a new Intersection Observer instance
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+    });
+
+    // Start observing the target element
+    observer.observe(targetElement);
+  });
 }
 
